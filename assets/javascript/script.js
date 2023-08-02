ae996759d4a1414d2ece8c81b8fd299347ad50fc
IgnacioG08 <134964013+IgnacioG08@users.noreply.github.com>
2023-08-02 16:35:51 -0700

    add time class and save text to localStorage

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,46 +1,58 @@
 $(function () {
 
-// elements
-var currentDayEl = $("#currentDay")
+  // functions
 
-// state
-var dateInterval;
+  function init() {
+    startTime();
+    for (var i = 9; i <= 17; i++) {
+      var text = localStorage.getItem("hour-" + i);
+      $("#hour-" + i + " .description").val(text);
+    }
+  }
 
+  function updateDateDisplay() {
+    var today = dayjs()
 
-// functions
+    $("#currentDay").text(today.format("dddd[,] MMMM DD[,] YYYY"));
+    var hour = today.hour();
+    $(".time-block").each(function () {
+      var blockHour = Number($(this).attr("id").substring(5));
+      if (blockHour < hour) {
+        $(this).addClass("past");
+        $(this).removeClass("present");
+        $(this).removeClass("future");
+      }
+      else if (blockHour == hour) {
+        $(this).addClass("present");
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+      }
+      else {
+        $(this).addClass("future");
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+      }
+    });
+  }
 
-function init() {
-  startTime();
-}
+  function startTime() {
+    updateDateDisplay()
+    var dateInterval = setInterval(function () {
+      updateDateDisplay();
 
-function updateDateDisplay() {
-  var today = dayjs()
+    }, 1000)
+  }
 
-  currentDayEl.text(today.format("dddd[,] MMMM DD[,] YYYY"));
-}
+  // Event listeners
+  $(".saveBtn").on("click", function () {
+    var hour = $(this).parent().attr("id");
+    var text = $(this).prev().val();
+    localStorage.setItem(hour, text);
 
-function startTime() {
-  updateDateDisplay()
-  dateInterval = setInterval(function() {
-    updateDateDisplay();
+  });
 
-  },)
-}
+  // initialize
 
-function currentTime() {
-
-}
-
-// Event listeners
-
-document.getElementById("saveData").addEventListener("click", function() {
-
-});
-
-
-
-// initialize
-
-init();
+  init();
 
 });
